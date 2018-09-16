49e0c0219c2bfe94d07ed9114da38b0e6430b29a
Benjamin Brahmer <info@b-brahmer.de>
2018-09-16 19:02:52 +0200

    gui now with mainwindow and a lineedit

--- mediamanager/gui.py
+++ mediamanager/gui.py
@@ -2,14 +2,28 @@
 """ The gui for mediamanager """
 import sys
 
-from PySide2.QtWidgets import QApplication, QLabel
+from PySide2.QtWidgets import QApplication, QMainWindow, QToolBar, QLineEdit
+
+
+class MainWindow(QMainWindow):
+    """docstring for MainWindow"""
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('The Python Media Manager')
+        self.toolBar = QToolBar()
+        self.toolBar.setMovable(False)
+        self.addToolBar(self.toolBar)
+        self.addressLineEdit = QLineEdit()
+        self.toolBar.addWidget(self.addressLineEdit)
+
+
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
-    label = QLabel("Hello World")
-
-    label.show()
-
+    mainWin = MainWindow()
+    availableGeometry = app.desktop().availableGeometry(mainWin)
+    mainWin.resize(availableGeometry.width() * 2 / 3, availableGeometry.height() * 2 / 3)
+    mainWin.show()
     sys.exit(app.exec_())
