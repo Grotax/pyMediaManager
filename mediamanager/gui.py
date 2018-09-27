fc2cb31889d174ac79acac0b3e098303276800fa
Benjamin Brahmer <info@b-brahmer.de>
2018-09-27 14:02:30 +0200

    minimial progress on the ui

--- mediamanager/gui.py
+++ mediamanager/gui.py
@@ -2,7 +2,25 @@
 """ The gui for mediamanager """
 import sys
 
-from PySide2.QtWidgets import QApplication, QMainWindow, QToolBar, QLineEdit
+from PySide2.QtWidgets import QApplication, QMainWindow, QToolBar, QLineEdit, QGridLayout, QBoxLayout, QWidget
+
+class DetailView(QBoxLayout):
+    """docstring for DetailView"""
+    def __init__(self):
+        super().__init__()
+
+class OverView(QGridLayout):
+    """docstring for OverView"""
+    def __init__(self):
+        super().__init__()
+
+
+class Search(QBoxLayout):
+    """docstring for Search"""
+    def __init__(self):
+        super().__init__(QBoxLayout.LeftToRight)
+        self.searchbar = QLineEdit()
+        self.addWidget(self.searchbar)
 
 
 class MainWindow(QMainWindow):
@@ -10,11 +28,21 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('The Python Media Manager')
-        self.toolBar = QToolBar()
-        self.toolBar.setMovable(False)
-        self.addToolBar(self.toolBar)
-        self.addressLineEdit = QLineEdit()
-        self.toolBar.addWidget(self.addressLineEdit)
+        self.main_widget = QWidget()
+        self.main_grid = QGridLayout()
+
+        self.searchbar = QLineEdit()
+        self.main_grid.addWidget(self.searchbar, 1, 0)
+
+        self.main_widget.setLayout(self.main_grid)
+
+        self.setCentralWidget(self.main_widget)
+
+        #self.over_view = OverView()
+        #self.search = Search()
+        #self.main_grid.addItem(self.search, 0, 0)
+
+        #self.main_grid.addItem(self.over_view, 0, 1)
 
 
 
